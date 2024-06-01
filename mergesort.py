217f495baaa02b00ce8c9aa481708c3799421db0
eKlerner <e.klerner@stud.uni-goettingen.de>
2024-06-01 15:41:43 +0200

    Kommentiert mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,70 @@
+# Funktion zum Zuweisen von Werten von einer Liste zu einer anderen an bestimmten Indizes
 def ASSIGNMENT(new_list, i, old_list, j):
     new_list[i] = old_list[j]
 
+# Funktion zur Implementierung des Merge-Sort-Algorithmus
+def mergeSort(liste_zum_sortieren):
+    # Überprüfen, ob die Liste mehr als ein Element hat
+    if len(liste_zum_sortieren) > 1:
+        # Finden des mittleren Index der Liste
+        mitte = len(liste_zum_sortieren) // 2
+        
+        # Die Liste in zwei Hälften aufteilen
+        links = liste_zum_sortieren[:mitte]
+        rechts = liste_zum_sortieren[mitte:]
 
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
+        # Rekursiver Aufruf von mergeSort für die linke und rechte Hälfte
+        mergeSort(links)
+        mergeSort(rechts)
 
-        mergeSort(left)
-        mergeSort(right)
-
+        # Zusammenführen der sortierten Hälften
         l = 0
         r = 0
         i = 0
-
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
+        
+        # Zusammenführen der beiden Hälften unter Beibehaltung der sortierten Reihenfolge
+        while l < len(links) and r < len(rechts):
+            if links[l] <= rechts[r]:
+                ASSIGNMENT(new_list=liste_zum_sortieren, i=i, old_list=links, j=l)
                 l += 1
             else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
+                ASSIGNMENT(new_list=liste_zum_sortieren, i=i, old_list=rechts, j=r)
                 r += 1
             i += 1
 
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
+        # Hinzufügen der verbleibenden Elemente aus der linken Hälfte
+        while l < len(links):
+            liste_zum_sortieren[i] = links[l]
             l += 1
             i += 1
 
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
+        # Hinzufügen der verbleibenden Elemente aus der rechten Hälfte
+        while r < len(rechts):
+            liste_zum_sortieren[i] = rechts[r]
             r += 1
             i += 1
 
-
+# Importieren der matplotlib-Bibliothek für das Plotten
 import matplotlib.pyplot as plt
 
-my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
+# Definition der unsortierten Liste
+meine_liste = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+
+# Plotten der unsortierten Liste
+x = range(len(meine_liste))
+plt.plot(x, meine_liste)
+plt.title('Unsortierte Liste')
+plt.xlabel('Index')
+plt.ylabel('Wert')
 plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
+
+# Sortieren der Liste mit Merge-Sort
+mergeSort(meine_liste)
+
+# Plotten der sortierten Liste
+x = range(len(meine_liste))
+plt.plot(x, meine_liste)
+plt.title('Sortierte Liste')
+plt.xlabel('Index')
+plt.ylabel('Wert')
 plt.show()
